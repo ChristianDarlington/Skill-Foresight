f31f40647b270aa1c2a3dfd1c798590e9770080d
Christian <christiandarlington0@gmail.com>
2023-06-29 01:17:03 -0400

    chatbox is functional

--- src/Components/Chatbox.js
+++ src/Components/Chatbox.js
@@ -4,99 +4,97 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
 
  
-const API_KEY = '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' 
+const API_KEY = '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' 
+
+const systemMessage = {
+  role: 'system',
+  content: "Speak like a Career expert, helping people of all ages find their career path."
+}
 
 const Chatbox = () => {
-
-
 
   const [typing, setTyping] = useState(false)
   const [messages, setMessages] = useState([
     {
     message: 'Hello, I\'m Career Finder!',
-    sentTime: 'just now',
-    sender: 'Career Expert'
+    role: 'system',
+    direction: 'incoming'
   }
 ])
-  // const [message, setMessage] =  useState('')
+
 
   
 
-  const handleSend = async(message) => {
-      const newMessage = {
-        message: message,
-        sender: 'user',
-        direction: 'outgoing'
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+    };
+
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
+
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
+    setTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
       }
-      const newMessages = [...messages, newMessage] // all the old messages + the new message 
+      return { role: role, content: messageObject.message}
+    });
 
-      // update our messages state
-      setMessages(newMessages)
-      
-      // set a typing indicator (Chat is typing)
-      setTyping(true)
 
-      // process message to demo (send it over and see the response)
-      await processMessageToDemo(newMessages)
-  }
-  
-  
-  async function processMessageToDemo(chatmessages) {
-    // chatMessages {sender: 'user' or 'ChatGPT', message: "The message content here"}
-    // apiMessages {role: 'user' or 'assistant', content: 'The message content here'}
-
-    let apiMessages = chatmessages.map((messageObject) => {
-        let role =''
-        if(messageObject === 'ChatGPT'){
-          role = 'assistant'
-        } else {
-          role= 'user'
-        }
-        return{role: role, content: messageObject.message}
-    })
-
-      // 'system' -> generally one initial message defining HOW we wnat a chatgpt to talk
-
-    const systemMessage = {
-      role: "system",
-      content: "Speak like a Career expert, helping people of all ages find their career path."
-    }
-
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatGPT to act. 
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
-      "message": [
-        systemMessage,
-        ...apiMessages // [message1, message2, messgae3]
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatGPT
+        ...apiMessages // The messages from our chat with ChatGPT
       ]
     }
 
-
-   
-    await fetch(" https://api.openai.com/v1/chat/completions",{
-      method: 'POST',
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
       headers: {
         "Authorization": "Bearer " + API_KEY,
         "Content-Type": "application/json"
-      }, 
+      },
       body: JSON.stringify(apiRequestBody)
     }).then((data) => {
-      return data.json()
+      return data.json();
     }).then((data) => {
-      console.log(data)
-      setMessages(
-      [...chatmessages, {
+      console.log(data);
+      if(data.choices && data.choices.length > 0){
+      setMessages([...chatMessages, {
         message: data.choices[0].message.content,
-        sender: 'ChatGPT'
-      }]
-      )
-      setTyping(false)
-    })
+        sender: "ChatGPT"
+      }]);
+    } else {
+      setMessages([...chatMessages, {
+        message: 'Cannot answer question, please refresh page and try again',
+        sender: "ChatGPT"
+      }])
+    }
+      setTyping(false);
+    });
   }
- 
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();  
-// }
 
  
  
